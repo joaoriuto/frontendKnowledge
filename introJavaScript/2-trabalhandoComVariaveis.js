2973f1ae57cbc253679a7eff8a35ddb78e0a8077
João Riuto <jr00.dat@gmail.com>
2021-02-08 01:04:13 -0300

    Um pouco mais sobre o comportamento JS

---
+++ introJavaScript/2-trabalhandoComVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com variáveis.");
+// JS é case sensitive <-------
+
+/** Explains:
+ * 
+ * -> Devemos seguir um padrão quando formos declarar variáveis ou constantes.
+ * -> JavaScript infere o tipo de variável de acordo com a sua composição.
+ * -> Sempre declarar a variável como const ou let, para que assim não fiquem
+ *      no escopo global.
+ * -------> Exemplo: <-------
+ * mes = "Janeiro" (errado) 
+ * const mes = "Janeiro" (Certo). 
+ * 
+ */
+
---
+++ introJavaScript/3-operacoesAritmeticas.js
@@ -0,0 +1,7 @@
+console.log("Operações aritméticas:");
+
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+
+console.log("ano" + 2021);
+console.log("2" + "2");
