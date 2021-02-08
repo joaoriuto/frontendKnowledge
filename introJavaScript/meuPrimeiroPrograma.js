8e1152b90fd86036f73b1a334e898338480c2084
João Riuto <jr00.dat@gmail.com>
2021-02-07 22:09:45 -0300

    Introducao ao JavaScript - primeiro arquivo

---
+++ introJavaScript/meuPrimeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro programa");
+
+const idade = 27;
+const nome = "João Riuto";
+const soma = idade + 5;
+const saldacao = "Olá, bom dia ";
+
+console.log(saldacao + nome + " a sua idade é: " + idade 
++ " somada com 5, resulta em: " + soma);
